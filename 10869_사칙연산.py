c03c960315a92311fa4884c509adbd68fc2cb302
sangjae84 <dltkdwo0514@gmail.com>
2023-06-09 12:41:32 +0900

    .

---
+++ 10869_사칙연산.py
@@ -0,0 +1,10 @@
+a,b = map(int,input().split())
+
+print(a + b) #  첫째 줄 : a + b
+print(a - b) #  둘째 줄 : a - b
+print(a * b) #  셋째 줄 : a * b
+print(a // b) #  넷째 줄 : a // b
+print(a % b) #  다섯쨰 줄 : a % b
+
+
+
